95a5a4215c6dfc6462015ccb2146fa57ea0b1204
1-3127 <gnwu1234567@gmail.com>
2026-08-28 23:56:28 +0900

    fix: force Qt event loop exit when windows close

--- WIP/main.py
+++ WIP/main.py
@@ -16,6 +16,7 @@ from startup_manager import StartupManager
 
 def main() -> int:
     application = QApplication(sys.argv)
+    application.setQuitOnLastWindowClosed(True)
     install_fatal_exception_handler(application)
     QCoreApplication.setOrganizationName("ImageDiary")
     QCoreApplication.setApplicationName("ImageDiary")
@@ -37,6 +38,7 @@ def main() -> int:
 
     window = MainWindow(settings_repository, StartupManager())
     application.aboutToQuit.connect(window.shutdown)
+    application.lastWindowClosed.connect(lambda: application.exit(0))
     window.show()
     QTimer.singleShot(0, window.check_for_recovery)
     if cleanup_error is not None:
